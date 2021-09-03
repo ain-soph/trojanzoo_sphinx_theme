4c046f27e55977d740e6e8762d6a0d49c10483dd
ain-soph <ain-soph@live.com>
2021-09-03 00:02:18 -0400

    update setup

--- trojanzoo_sphinx_theme/__init__.py
+++ trojanzoo_sphinx_theme/__init__.py
@@ -14,6 +14,8 @@ def get_html_theme_path():
     cur_dir = path.abspath(path.dirname(path.dirname(__file__)))
     return cur_dir
 
-# See http://www.sphinx-doc.org/en/stable/theming.html#distribute-your-theme-as-a-python-package
+
 def setup(app):
-    app.add_html_theme('trojanzoo_sphinx_theme', path.abspath(path.dirname(__file__)))
+    # See https://www.sphinx-doc.org/en/master/development/theming.html
+    app.add_html_theme('trojanzoo_sphinx_theme',
+                       path.abspath(path.dirname(__file__)))
